781bf481060783e7857b56e00a3ca2855d7d9c7e
Andrea Battagla (Red Hat) <abattagl@redhat.com>
2016-07-25 20:19:51 +0200

    added EAP7 config

---
+++ eap7-config/add-users.sh
@@ -0,0 +1,3 @@
+./add-user.sh -r ManagementRealm -u admin -p Pa$$w0rd.01
+./add-user.sh -r ApplicationRealm -u jmsuser -p Pa$$w0rd.01 -g jmsuser
+./add-user.sh -r ApplicationRealm -u corebridgeuser -p Pa$$w0rd.01 -g corebridgeuser
